706d713e41c97daeeaf4212a30d508e053b14e4b
Roy Hanley <royhanley8@gmail.com>
2023-03-23 15:15:04 +0000

    Speeded up mergeListsOfObjects function

--- helpers/mergeLists.js
+++ helpers/mergeLists.js
@@ -1,5 +1,3 @@
-
-
 function mergeListsOfObjects(a, b) {
   /**
    * Merges two lists of objects removing duplicates based upon object id.
@@ -11,9 +9,11 @@ function mergeListsOfObjects(a, b) {
    * [{"id": 1}, {"id": 2}, {"id": 3}, {"id": 4}, {"id": 5}]
    */
   const newList = [...a];
+  const ids = newList.map(i => i.id);
   b.forEach(item => {
-    if (!newList.map(i => i.id).includes(item.id)) {
-      newList.push(item)
+    if (!ids.includes(item.id)) {
+      newList.push(item);
+      ids.push(item.id);
     }
   })
   return newList;
