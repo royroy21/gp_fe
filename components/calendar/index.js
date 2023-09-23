171bc7cf07c939387f8b7077957f4586a4e4953e
Roy Hanley <royhanley8@gmail.com>
2023-09-23 23:44:16 +0100

    Added candelar model fixed height.

--- components/calendar/index.js
+++ components/calendar/index.js
@@ -3,6 +3,7 @@ import CalendarPicker from "react-native-calendar-picker";
 import {useTheme} from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import CenteredModalWithOneButton from "../centeredModal/CenteredModalWithOneButton";
+import {View} from "react-native";
 
 function CalendarModal({visible, date, setDate, onRequestClose}) {
   const theme = useTheme()
@@ -10,42 +11,44 @@ function CalendarModal({visible, date, setDate, onRequestClose}) {
   const windowWidth = Dimensions.get("window").width;
   return (
     <CenteredModalWithOneButton showModal={visible} setModal={onRequestClose}>
-      <CalendarPicker
-        initialDate={now}
-        selectedDate={date ? new Date(date) : undefined}
-        minDate={now}
-        width={Math.round(windowWidth*0.9)}  // 90% of screen
-        textStyle={{
-          color: theme.palette.primary.main,
-        }}
-        todayBackgroundColor={theme.palette.primary.main}
-        todayTextStyle={{color: theme.palette.background.main}}
-        selectedDayStyle={{
-          backgroundColor: theme.palette.primary.main,
-          color: theme.palette.background.main,
-        }}
-        restrictMonthNavigation={true}
-        headerWrapperStyle={{marginTop: 10}}
-        style={styles.calendar}
-        nextComponent={
-          <Icon
-            name="chevron-right"
-            size={25}
-            color={theme.palette.secondary.main}
-          />
-        }
-        previousComponent={
-          <Icon
-            name="chevron-left"
-            size={25}
-            color={theme.palette.secondary.main}
-          />
-        }
-        onDateChange={(date) => {
-          setDate(date)
-          onRequestClose()
-        }}
-      />
+      <View style={styles.container}>
+        <CalendarPicker
+          initialDate={now}
+          selectedDate={date ? new Date(date) : undefined}
+          minDate={now}
+          width={Math.round(windowWidth*0.9)}  // 90% of screen
+          textStyle={{
+            color: theme.palette.primary.main,
+          }}
+          todayBackgroundColor={theme.palette.primary.main}
+          todayTextStyle={{color: theme.palette.background.main}}
+          selectedDayStyle={{
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.background.main,
+          }}
+          restrictMonthNavigation={true}
+          headerWrapperStyle={{marginTop: 10}}
+          style={styles.calendar}
+          nextComponent={
+            <Icon
+              name="chevron-right"
+              size={25}
+              color={theme.palette.secondary.main}
+            />
+          }
+          previousComponent={
+            <Icon
+              name="chevron-left"
+              size={25}
+              color={theme.palette.secondary.main}
+            />
+          }
+          onDateChange={(date) => {
+            setDate(date)
+            onRequestClose()
+          }}
+        />
+      </View>
     </CenteredModalWithOneButton>
   )
 }
@@ -54,18 +57,7 @@ export default CalendarModal;
 
 const styles = StyleSheet.create({
   container: {
-    alignSelf: "center",
-    justifyContent: "center",
-    marginRight: 50,
-    marginLeft: 50,
-    borderRadius: 5,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    elevation: 5
+    height: 300,
   },
   closeButtonContainer: {
     justifyContent: "center",
