2172e07a1335d8deb48ae2af093b5235369319ef
roy <royhanley8@gmail.com>
2024-08-08 11:14:14 +0100

    Main menu button is now user image if thumbnail is available.

--- components/navigation/LoginOrMenuButton.js
+++ components/navigation/LoginOrMenuButton.js
@@ -2,6 +2,7 @@ import {Button, Icon, IconButton, Text} from "@react-native-material/core";
 import {Pressable, StyleSheet, View} from "react-native";
 import MainMenu from "../menu/MainMenu";
 import {useState} from "react";
+import Image from "../Image/Image";
 
 function UserNameButton({ username, navigation }) {
   const onPress = () => {
@@ -11,6 +12,26 @@ function UserNameButton({ username, navigation }) {
     <Pressable onPress={onPress}>
       <Text style={styles.username}>{username}</Text>
     </Pressable>
+  )
+}
+
+function MenuButton({ user, mainMenu, setMainMenu }) {
+  return (
+    user.thumbnail ? (
+      <Image
+        imageUri={user.image}
+        thumbnailUri={user.thumbnail}
+        smallerThumbnail={true}
+        onThumbnailPress={() => setMainMenu(!mainMenu)}
+        containerStyle={styles.image}
+        thumbnailStyle={styles.thumbnailStyle}
+      />
+    ) : (
+      <IconButton
+        icon={<Icon name="menu" size={30} />}
+        onPress={() => setMainMenu(!mainMenu)}
+      />
+    )
   )
 }
 
@@ -29,12 +50,10 @@ function LoginOrMenuButton({ user, isWeb, isSmallScreen, navigation, route }) {
         setMainMenu={setMainMenu}
         navigation={navigation}
       />
-      {user && isWeb && !isSmallScreen && <UserNameButton username={user.username} navigation={navigation} />}
+      {/* TODO - maybe move search button/icon here? */}
+      {/*{user && isWeb && !isSmallScreen && <UserNameButton username={user.username} navigation={navigation} />}*/}
       {user ? (
-      <IconButton
-        icon={<Icon name="menu" size={30} />}
-        onPress={() => setMainMenu(!mainMenu)}
-      />
+      <MenuButton user={user} mainMenu={mainMenu} setMainMenu={setMainMenu} />
     ) : (
       <Button
         title={"login"}
@@ -57,6 +76,14 @@ const styles = StyleSheet.create({
   },
   loginButton: {
     marginRight: 10,
+  },
+  image: {
+    marginRight: 15,
+  },
+  thumbnailStyle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
   },
 });
 
